f7fb703233d01d03c60805a2f1d4c0167a5d5fb0
Ruben Vazquez-Chapa <ravc@protonmail.com>
2019-10-25 09:38:21 -0500

    Add basic server files

---
+++ Server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('test')
+def handle_test(message):
+    send(message)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
